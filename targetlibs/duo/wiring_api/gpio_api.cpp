5a8097f11ae2fb732a327228ecf96e917f0e2e5c
XuGuohui <guohui@redbear.cc>
2016-03-24 14:25:05 +0800

    Keep the Arduino function names, except the analogWrite()

--- targetlibs/duo/wiring_api/gpio_api.cpp
+++ targetlibs/duo/wiring_api/gpio_api.cpp
@@ -5,64 +5,14 @@ extern "C" {
 #endif
 
 
-void gpio_pinMode(uint16_t pin, PinMode mode)
-{
-	pinMode(pin, mode);
-}
-
-PinMode gpio_getPinMode(uint16_t pin)
-{
-	return getPinMode(pin);
-}
-
-bool gpio_pinAvailable(uint16_t pin)
-{
-    return pinAvailable(pin);
-}
-
-void gpio_setADCSampleTime(uint8_t ADC_SampleTime)
-{
-	setADCSampleTime(ADC_SampleTime);
-}
-
-int32_t gpio_analogRead(uint16_t pin)
-{
-    return analogRead(pin);
-}
-
-void gpio_analogWrite(uint16_t pin, uint16_t value)
+void wiring_analogWrite(uint16_t pin, uint16_t value)
 {
 	analogWrite(pin, value);
 }
 
-void gpio_digitalWrite(uint16_t pin, uint8_t value)
+void wiring_analogWriteWithFreq(uint16_t pin, uint16_t value, uint16_t pwm_frequency)
 {
-	digitalWrite(pin, value);
-}
-
-int32_t gpio_digitalRead(uint16_t pin)
-{
-    return digitalRead(pin);
-}
-
-long gpio_map(long value, long fromStart, long fromEnd, long toStart, long toEnd)
-{
-    return map(value, fromStart, fromEnd, toStart, toEnd);
-}
-
-void gpio_shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t val)
-{
-	shiftOut(dataPin, clockPin, bitOrder, val);
-}
-
-uint8_t gpio_shiftIn(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder)
-{
-    return shiftIn(dataPin, clockPin, bitOrder);
-}
-
-uint32_t gpio_pulseIn(pin_t pin, uint16_t value)
-{
-     return pulseIn(pin, value);
+	analogWrite(pin, value, pwm_frequency);
 }
 
 
--- targetlibs/duo/wiring_api/gpio_api.h
+++ targetlibs/duo/wiring_api/gpio_api.h
@@ -3,23 +3,24 @@
 
 #include "pinmap_hal.h"
 
-void     gpio_pinMode(uint16_t pin, PinMode mode);
-PinMode  gpio_getPinMode(uint16_t pin);
-bool     gpio_pinAvailable(uint16_t pin);
+void     pinMode(uint16_t pin, PinMode mode);
+PinMode  getPinMode(uint16_t pin);
+bool     pinAvailable(uint16_t pin);
 
-void     gpio_setADCSampleTime(uint8_t ADC_SampleTime);
-int32_t  gpio_analogRead(uint16_t pin);
-void     gpio_analogWrite(uint16_t pin, uint16_t value);
+void     setADCSampleTime(uint8_t ADC_SampleTime);
+int32_t  analogRead(uint16_t pin);
+void     wiring_analogWrite(uint16_t pin, uint16_t value);
+void     wiring_analogWriteWithFreq(uint16_t pin, uint16_t value, uint16_t pwm_frequency);
 
-void     gpio_digitalWrite(uint16_t pin, uint8_t value);
-int32_t  gpio_digitalRead(uint16_t pin);
+void     digitalWrite(uint16_t pin, uint8_t value);
+int32_t  digitalRead(uint16_t pin);
 
-long     gpio_map(long value, long fromStart, long fromEnd, long toStart, long toEnd);
+long     map(long value, long fromStart, long fromEnd, long toStart, long toEnd);
 
-void     gpio_shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t val);
-uint8_t  gpio_shiftIn(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder);
+void     shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t val);
+uint8_t  shiftIn(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder);
 
-uint32_t gpio_pulseIn(pin_t pin, uint16_t value);
+uint32_t pulseIn(pin_t pin, uint16_t value);
 
 
 #endif
