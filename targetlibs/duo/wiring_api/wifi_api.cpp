a33ed052c4b0de16956d781b524e22dc5391a1c8
XuGuohui <guohui@redbear.cc>
2016-03-25 16:21:59 +0800

    Use spark namespace to reference WiFi object

--- targetlibs/duo/wiring_api/wifi_api.cpp
+++ targetlibs/duo/wiring_api/wifi_api.cpp
@@ -4,137 +4,138 @@
 extern "C" {
 #endif
 
+using namespace spark;
 
 WLanConfig* wifi_wifiConfig(void)
 {
-    return spark::WiFi.wifi_config();
+    return WiFi.wifi_config();
 }
 
 void wifi_macAddress(uint8_t *mac)
 {
-	spark::WiFi.macAddress(mac);
+	WiFi.macAddress(mac);
 }
 
 void wifi_localIP(uint32_t *local_ip)
 {
     IPAddress IP;
-    IP = spark::WiFi.localIP();
+    IP = WiFi.localIP();
     *local_ip = IP;
 }
 
 void wifi_subnetMask(uint32_t *net_mask)
 {
     IPAddress IP;
-    IP = spark::WiFi.subnetMask();
+    IP = WiFi.subnetMask();
     *net_mask = IP;
 }
 
 void wifi_gatewayIP(uint32_t *gateway_ip)
 {
     IPAddress IP;
-    IP = spark::WiFi.gatewayIP();
+    IP = WiFi.gatewayIP();
     *gateway_ip = IP;
 }
 
 void wifi_dnsServerIP(uint32_t *dns_ip)
 {
     IPAddress IP;
-    IP = spark::WiFi.dnsServerIP();
+    IP = WiFi.dnsServerIP();
     *dns_ip = IP;
 }
 
 void wifi_dhcpServerIP(uint32_t *dhcp_ip)
 {
     IPAddress IP;
-    IP = spark::WiFi.dhcpServerIP();
+    IP = WiFi.dhcpServerIP();
     *dhcp_ip = IP;
 }
 
 void wifi_BSSID(uint8_t* bssid)
 {
-	spark::WiFi.BSSID(bssid);
+	WiFi.BSSID(bssid);
 }
 
 const char *SSID(void)
 {
-    return spark::WiFi.SSID();
+    return WiFi.SSID();
 }
 
 int8_t wifi_RSSI(void)
 {
-    return spark::WiFi.RSSI();
+    return WiFi.RSSI();
 }
 
 uint32_t wifi_ping(uint32_t ip, uint8_t nTries)
 {
 	IPAddress remoteIP = ip;
-    return spark::WiFi.ping(remoteIP, nTries);
+    return WiFi.ping(remoteIP, nTries);
 }
 
 void wifi_connect(void)
 {
-	spark::WiFi.connect();
+	WiFi.connect();
 }
 
 void wifi_disconnect(void)
 {
-	spark::WiFi.disconnect();
+	WiFi.disconnect();
 }
 
 bool wifi_isConnecting(void)
 {
-    return spark::WiFi.connecting();
+    return WiFi.connecting();
 }
 
 bool wifi_isReady(void)
 {
-    return spark::WiFi.ready();
+    return WiFi.ready();
 }
 
 void wifi_on(void)
 {
-	spark::WiFi.on();
+	WiFi.on();
 }
 
 void wifi_off(void)
 {
-	spark::WiFi.off();
+	WiFi.off();
 }
 
 void wifi_startListen(void)
 {
-	spark::WiFi.listen();
+	WiFi.listen();
 }
 
 void wifi_stopListen(void)
 {
-	spark::WiFi.listen(false);
+	WiFi.listen(false);
 }
 
 bool wifi_isListening(void)
 {
-    return spark::WiFi.listening();
+    return WiFi.listening();
 }
 
 void wifi_setCredentials(const char *ssid, const char *password, unsigned long security)
 {
-	spark::WiFi.setCredentials(ssid, password, security);
+	WiFi.setCredentials(ssid, password, security);
 }
 
 bool wifi_hasCredentials(void)
 {
-    return spark::WiFi.hasCredentials();
+    return WiFi.hasCredentials();
 }
 
 bool wifi_clearCredentials(void)
 {
-    return spark::WiFi.clearCredentials();
+    return WiFi.clearCredentials();
 }
 
 void resolve(const char* name, uint32_t *ip)
 {
     IPAddress IP;
-    IP = spark::WiFi.resolve(name);
+    IP = WiFi.resolve(name);
     *ip = IP;
 }
 
@@ -151,27 +152,27 @@ void wifi_setStaticIP(uint32_t host_ip, uint32_t netmask_ip, uint32_t gateway_ip
     gateway = gateway_ip;
     dns = dns_ip;
 
-    spark::WiFi.setStaticIP(_host, _netmask, _gateway, _dns);
+    WiFi.setStaticIP(_host, _netmask, _gateway, _dns);
 }
 
 void wifi_useStaticIP(void)
 {
-	spark::WiFi.useStaticIP();
+	WiFi.useStaticIP();
 }
 
 void wifi_useDynamicIP(void)
 {
-	spark::WiFi.useDynamicIP();
+	WiFi.useDynamicIP();
 }
 
 int wifi_scan(WiFiAccessPoint* results, size_t result_count)
 {
-    return spark::WiFi.scan(results, result_count);
+    return WiFi.scan(results, result_count);
 }
 
 int wifi_getCredentials(WiFiAccessPoint* results, size_t result_count)
 {
-    return spark::WiFi.getCredentials(results, result_count);
+    return WiFi.getCredentials(results, result_count);
 }
 
 
