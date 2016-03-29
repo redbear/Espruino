5b34f095ba1c83efbfcc5ee2debf424bdfde0307
XuGuohui <guohui@redbear.cc>
2016-03-29 17:41:50 +0800

    Update the board.py

--- boards/REDBEARDUO.py
+++ boards/REDBEARDUO.py
@@ -18,7 +18,9 @@ import pinutils;
 info = {
  'name' : "RedBear Duo",
  'link' : [ "http://www.RedBear.cc/Duo" ],
- 'variables' : 2000,
+ 'default_console' : "EV_SERIAL1",
+ 'variables' : 2250,
+ 'bootloader' : 0,
  'binary_name' : 'espruino_%v_redbearduo.bin',
  'build' : {
   'defines' : [
@@ -32,14 +34,25 @@ chip = {
   'ram' : 128,
   'flash' : 1024,
   'speed' : 120,
-  'usart' : 3,
+  'usart' : 2,
   'spi' : 2,
-  'i2c' : 2,
-  'adc' : 6,
-  'dac' : 0
+  'i2c' : 1,
+  'adc' : 8,
+  'dac' : 2,
+  'saved_code' : {
+    'address' : 0x080E0000, # last page start address
+    'page_size' : 131072, # size of pages
+    'pages' : 1, # number of pages we're using
+    'flash_available' : 256 # 256KB internal flash used for user part
+  },
 };
 
 devices = {
+  'LED1' : { 'pin' : 'B11' }, # R
+  'LED2' : { 'pin' : 'B1' },  # G
+  'LED3' : { 'pin' : 'B0' },  # B
+  'LED4' : { 'pin' : 'A13' }, # user led
+  'BTN1' : { 'pin' : 'B2' },
 };
 
 
